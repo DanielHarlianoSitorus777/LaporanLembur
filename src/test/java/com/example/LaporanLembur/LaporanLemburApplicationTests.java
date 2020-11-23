0d847260b78695b97b138418afe690f552a5988e
Daniel Harliano Sitorus <harlianositorusdaniel@gmail.com>
2020-11-23 13:10:22 +0700

    First Commit

---
+++ src/test/java/com/example/LaporanLembur/LaporanLemburApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LaporanLembur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaporanLemburApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
